6d21c757c93f4a7afb1d04faf89e2b91d44e3b1a
oliverio89 <oliverio89@gmail.com>
2023-05-10 15:47:42 +0200

    nueva prueba

--- src/components/NewList/newList.jsx
+++ src/components/NewList/newList.jsx
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./newList.css";
 import BotonTask from "../BotonTask/botonTask";
 
 function NewList() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleInputChange = (e) => {
+    setNewTask(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (newTask.trim() !== "") {
+      setTasks([...tasks, newTask]);
+      setNewTask("");
+    }
+  };
+
   return (
     <section className="card">
       <h3>List 1</h3>
       <div className="newTask">
-        <input
-          type="text"
-          className="nameTask"
-          id="inputTask"
-          placeholder="Name Task"
-        />
-        <BotonTask />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="nameTask"
+            id="inputTask"
+            placeholder="Name Task"
+            value={newTask}
+            onChange={handleInputChange}
+          />
+
+          <button className="btnnewTask">New Task2</button>
+          {/* <BotonTask /> */}
+        </form>
       </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
     </section>
   );
 }
