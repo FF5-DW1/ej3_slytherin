34720a455b9f3efc6527b2e509ffbf550514f7a1
ivon4 <git@github.com>
2023-05-10 12:21:21 +0200

    cambiosivon

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+// import Title from "./Title";
+// import Image from "./image"
+// import BodyContent from "./BodyContent";
+
+// function Card(props){
+// return(
+//     <article className="card">
+//       <Image image={props.img} alt={props.title} />
+
+//      {/* <Title title={props.title}/> */}
+//      {/* <bodyContent text={Props.body}/> */}
+//       {/* <p>{props.body}</p> */}
+//         <button>{props.btn ?? "View more"}</button>
+//     </article>
+// )
+// }
+// export default Card;
