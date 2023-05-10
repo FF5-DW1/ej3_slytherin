0ac1ae2bb5264bfdeb195708899a9faa3ba2532d
A3C2112 <anderson_2192@yahoo.com>
2023-05-10 13:03:27 +0200

    avance+ anderson

--- src/components/BotonList/botonList.jsx
+++ src/components/BotonList/botonList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function BotonList() {
-  return <div> BotonList</div>;
+  return <button className="btnnewList">New List</button>;
 }
 
 export default BotonList;
--- src/components/CardList/cardList.jsx
+++ src/components/CardList/cardList.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import "./cardList.css";
+import BotonList from "../BotonList/botonList";
 
 function CardList() {
   return (
     <section className="card">
       <h1>To Do List</h1>
       <div className="newList">
-        <p className="nameList">Name List</p>
-        <button className="btnnewList">New List</button>
+        <input
+          type="text"
+          className="nameList"
+          id="inputList"
+          placeholder="Name List"
+        />
+        <BotonList />
       </div>
       <hr />
+      <h3>List 1</h3>
+      <div className="newTask">
+        <input
+          type="text"
+          className="nameTask"
+          id="inputTask"
+          placeholder="Name Task"
+        />
+        <button className="btnnewTask">New Task</button>
+      </div>
     </section>
   );
 }
