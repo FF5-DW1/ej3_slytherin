f41c2c8e24cdda70c7ba9c7355357ab46ec55412
A3C2112 <anderson_2192@yahoo.com>
2023-05-10 11:22:43 +0200

    avance anderson

--- src/components/CardList/cardList.jsx
+++ src/components/CardList/cardList.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import "./cardList.css";
 
 function CardList() {
-  return <div>cardList</div>;
+  return (
+    <section className="card">
+      <h1>To Do List</h1>
+      <div className="newList">
+        <p className="nameList">Name List</p>
+        <button className="btnnewList">New List</button>
+      </div>
+      <hr />
+    </section>
+  );
 }
 
 export default CardList;
