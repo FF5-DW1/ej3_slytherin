de8f953532822c0f780681646048da20f229e37c
ivon4 <git@github.com>
2023-05-10 13:07:16 +0200

    otros commit

--- src/components/CardList/cardList.jsx
+++ src/components/CardList/cardList.jsx
@@ -5,7 +5,10 @@ function CardList() {
   return (
     <section className="card">
       <h1>To Do List</h1>
+      
       <div className="newList">
+      <div> <h1>To Do List</h1> 
+      
         <p className="nameList">Name List</p>
         <button className="btnnewList">New List</button>
       </div>
