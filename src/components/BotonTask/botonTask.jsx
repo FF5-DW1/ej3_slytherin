1afae5dacda388f96105e8e2021efa95ff592044
oliverio89 <oliverio89@gmail.com>
2023-05-11 12:46:21 +0200

    first commit day

--- src/components/BotonTask/botonTask.jsx
+++ src/components/BotonTask/botonTask.jsx
@@ -2,16 +2,11 @@ import React from "react";
 import "./botonTask.css";
 
 function BotonTask() {
-<<<<<<< HEAD
   return (
     <button className="btnnewTask" type="submit">
       New Task
     </button>
   );
-=======
-  return <button className="btnnewTask">New Task</button>;
-  
->>>>>>> develop
 }
 
 export default BotonTask;
--- src/components/NewList/newList.jsx
+++ src/components/NewList/newList.jsx
@@ -31,22 +31,24 @@ function NewList() {
             value={newTask}
             onChange={handleInputChange}
           />
-<<<<<<< HEAD
-
-          <button className="btnnewTask">New Task2</button>
           {/* <BotonTask /> */}
-        </form>
-      </div>
-=======
-           {/* <BotonTask /> */}
-         <button className="btnnewTask">New Task</button> 
+          <button className="btnnewTask">New Task</button>
           {/* <button type="submit">Add Task</button>  */}
         </form>
-       
       </div>
 
->>>>>>> develop
       <ul>
+        {tasks.sort(function (a, b) {
+          if (a.task > b.task) {
+            return 1;
+          }
+          if (a.task < b.task) {
+            return -1;
+          }
+          // a must be equal to b
+          return 0;
+        })}
+        ;
         {tasks.map((task, index) => (
           <li key={index}>{task}</li>
         ))}
@@ -56,83 +58,3 @@ function NewList() {
 }
 
 export default NewList;
-
-
-
-
-
-
-
-
-
-
-// import React from "react";
-// import "./newList.css";
-// import BotonTask from "../BotonTask/botonTask";
-
-// function NewList() {
-//   return (
-//     <section className="card">
-//       <h3>List 1</h3>
-//       <div className="newTask">
-      
-//         <input
-//           type="text"
-//           className="nameTask"
-//           id="inputTask"
-//           placeholder="Name Task"
-//         />
-//         <BotonTask />
-//       </div>
-//     </section>
-//   );
-// }
-
-// export default NewList;
-// import React, { useState } from "react";
-// import "./newList.css";
-// import BotonTask from "../BotonTask/botonTask";
-
-// function NewList() {
-//   const [tasks, setTasks] = useState([]);
-//   const [newTask, setNewTask] = useState("");
-
-//   const handleInputChange = (e) => {
-//     setNewTask(e.target.value);
-//   };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     if (newTask.trim() !== "") {
-//       setTasks([...tasks, newTask]);
-//       setNewTask("");
-//     }
-//   };
-
-//   return (
-//     <section className="card">
-//       <h3>List 1</h3>
-//       <div className="newTask">
-//         <form onSubmit={handleSubmit}>
-//           <input
-//             type="text"
-//             className="nameTask"
-//             id="inputTask"
-//             placeholder="Name Task"
-//             value={newTask}
-//             onChange={handleInputChange}
-//           />
-//           <BotonTask />
-//         </form>
-//       </div>
-
-//       <ul>
-//         {tasks.map((task, index) => (
-//           <li key={index}>{task}</li>
-//         ))}
-//       </ul>
-//     </section>
-//   );
-// }
-
-// export default NewList;
