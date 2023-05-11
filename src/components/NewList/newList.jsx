3682df6996879fff4a6f1bdd6c3eb9ded51ea3af
ivon4 <git@github.com>
2023-05-11 09:41:41 +0200

    cambios ivon

--- src/components/NewList/newList.jsx
+++ src/components/NewList/newList.jsx
@@ -1,10 +1,105 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./newList.css";
 import BotonTask from "../BotonTask/botonTask";
+
+// Resto del código del componente NewList
+
+
+// function NewList() {
+//   const [tasks, setTasks] = useState([]);
+//   const [newTask, setNewTask] = useState("");
+//   const [sortedTasks, setSortedTasks] = useState([]);
+//   const [listTitle, setListTitle] = useState("");
+
+//   const handleInputChange = (e) => {
+//     setNewTask(e.target.value);
+//   };
+
+//   const handleTitleChange = (e) => {
+//     setListTitle(e.target.value);
+//   };
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     if (newTask.trim() !== "") {
+//       setTasks([...tasks, newTask]);
+//       setNewTask("");
+//     }
+//   };
+
+//   const handleTitleClear = () => {
+//     setListTitle("");
+//   };
+
+//   const handleTaskToggleComplete = (index) => {
+//     const updatedTasks = [...tasks];
+//     updatedTasks[index] = { ...updatedTasks[index], completed: !updatedTasks[index].completed };
+//     setTasks(updatedTasks);
+//   };
+
+//   const sortTasks = () => {
+//     const sorted = [...tasks].sort((a, b) => a.title.localeCompare(b.title));
+//     setSortedTasks(sorted);
+//   };
+
+//   useEffect(() => {
+//     sortTasks();
+//   }, [tasks]);
+
+//   return (
+//     <section className="card">
+//       <div className="listTitle">
+//         <input
+//           type="text"
+//           className="titleInput"
+//           placeholder="List Title"
+//           value={listTitle}
+//           onChange={handleTitleChange}
+//         />
+//         <button className="clearTitleButton" onClick={handleTitleClear}>
+//           Clear Title
+//         </button>
+//       </div>
+//       <div className="newTask">
+//         <form onSubmit={handleSubmit}>
+//           <input
+//             type="text"
+//             className="nameTask"
+//             id="inputTask"
+//             placeholder="Name Task"
+//             value={newTask}
+//             onChange={handleInputChange}
+//           />
+//              <BotonTask />
+//           {/* <button type="submit">Add Task</button> */}
+//         </form>
+     
+//       </div>
+
+//       <ul>
+//         {sortedTasks.map((task, index) => (
+//           <li
+//             key={index}
+//             className={task.completed ? "completed" : ""}
+//             onClick={() => handleTaskToggleComplete(index)}
+//           >
+//             {task}
+//           </li>
+//         ))}
+//       </ul>
+//     </section>
+//   );
+// }
+
+// export default NewList;
+
+
+
 
 function NewList() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
+  const [sortedTasks, setSortedTasks] = useState([]);
 
   const handleInputChange = (e) => {
     setNewTask(e.target.value);
@@ -17,6 +112,15 @@ function NewList() {
       setNewTask("");
     }
   };
+
+  const sortTasks = () => {
+    const sorted = [...tasks].sort((a, b) => a.localeCompare(b));
+    setSortedTasks(sorted);
+  };
+
+  useEffect(() => {
+    sortTasks();
+  }, [tasks]);
 
   return (
     <section className="card">
@@ -31,15 +135,14 @@ function NewList() {
             value={newTask}
             onChange={handleInputChange}
           />
-           {/* <BotonTask /> */}
-         <button className="btnnewTask">New Task</button> 
-          {/* <button type="submit">Add Task</button>  */}
+            <BotonTask />
+          {/* <button type="submit">Add Task</button> */}
         </form>
-       
+      
       </div>
 
       <ul>
-        {tasks.map((task, index) => (
+        {sortedTasks.map((task, index) => (
           <li key={index}>{task}</li>
         ))}
       </ul>
@@ -50,37 +153,108 @@ function NewList() {
 export default NewList;
 
 
-
-
-
-
-
-
-
-
-// import React from "react";
+// import React, { useState, useEffect } from "react";
 // import "./newList.css";
 // import BotonTask from "../BotonTask/botonTask";
 
 // function NewList() {
+//   const [tasks, setTasks] = useState([]);
+//   const [newTask, setNewTask] = useState("");
+//   const [sortedTasks, setSortedTasks] = useState([]);
+//   const [listTitle, setListTitle] = useState("");
+
+//   const handleInputChange = (e) => {
+//     setNewTask(e.target.value);
+//   };
+
+//   const handleTitleChange = (e) => {
+//     setListTitle(e.target.value);
+//   };
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     if (newTask.trim() !== "") {
+//       setTasks([...tasks, newTask]);
+//       setNewTask("");
+//     }
+//   };
+
+//   const handleTitleClear = () => {
+//     setListTitle("");
+//   };
+
+//   const handleTaskToggleComplete = (index) => {
+//     const updatedTasks = [...tasks];
+//     updatedTasks[index] = { ...updatedTasks[index], completed: !updatedTasks[index].completed };
+//     setTasks(updatedTasks);
+//   };
+
+//   const sortTasks = () => {
+//     const sorted = [...tasks].sort((a, b) => a.title.localeCompare(b.title));
+//     setSortedTasks(sorted);
+//   };
+
+//   useEffect(() => {
+//     sortTasks();
+//   }, [tasks]);
+
 //   return (
 //     <section className="card">
-//       <h3>List 1</h3>
-//       <div className="newTask">
-      
+//       <div className="listTitle">
 //         <input
 //           type="text"
-//           className="nameTask"
-//           id="inputTask"
-//           placeholder="Name Task"
+//           className="titleInput"
+//           placeholder="List Title"
+//           value={listTitle}
+//           onChange={handleTitleChange}
 //         />
+//         <button className="clearTitleButton" onClick={handleTitleClear}>
+//           Clear Title
+//         </button>
+//       </div>
+//       <div className="newTask">
+//         <form onSubmit={handleSubmit}>
+//           <input
+//             type="text"
+//             className="nameTask"
+//             id="inputTask"
+//             placeholder="Name Task"
+//             value={newTask}
+//             onChange={handleInputChange}
+//           />
+//           <button type="submit">Add Task</button>
+//         </form>
 //         <BotonTask />
 //       </div>
+
+//       <ul>
+//         {sortedTasks.map((task, index) => (
+//           <li
+//             key={index}
+//             className={task.completed ? "completed" : ""}
+//             onClick={() => handleTaskToggleComplete(index)}
+//           >
+//             {task}
+//           </li>
+//         ))}
+//       </ul>
 //     </section>
 //   );
 // }
 
 // export default NewList;
+
+
+
+
+
+
+
+
+
+
+
+
 // import React, { useState } from "react";
 // import "./newList.css";
 // import BotonTask from "../BotonTask/botonTask";
@@ -114,8 +288,10 @@ export default NewList;
 //             value={newTask}
 //             onChange={handleInputChange}
 //           />
-//           <BotonTask />
+//             <BotonTask />
+//           {/* <button type="submit">Add Task</button> */}
 //         </form>
+      
 //       </div>
 
 //       <ul>
@@ -123,6 +299,50 @@ export default NewList;
 //           <li key={index}>{task}</li>
 //         ))}
 //       </ul>
+//     </section>
+//   );
+// }
+
+// export default NewList;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+// import "./newList.css";
+// import BotonTask from "../BotonTask/botonTask";
+
+// function NewList() {
+//   return (
+//     <section className="card">
+//       <h3>List 1</h3>
+//       <div className="newTask">
+      
+//         <input
+//           type="text"
+//           className="nameTask"
+//           id="inputTask"
+//           placeholder="Name Task"
+//         />
+//         <BotonTask />
+//       </div>
 //     </section>
 //   );
 // }
