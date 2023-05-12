ddaae449eb1c5ca71234fa89564f1b7a67faac30
oliverio89 <oliverio89@gmail.com>
2023-05-12 11:23:13 +0200

    finish footer 2

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -7,17 +7,31 @@ const Footer = () => {
     <footer>
       <div className="copyright">
         © 2023 Copyright :
-        <a href="https://www.linkedin.com/in/victor-wdfs/">
+        <a href="https://www.linkedin.com/in/victor-wdfs/" target="_blank">
           @Víctor Moreno García
         </a>
-        <a href="https://www.linkedin.com/in/anderson-almendarez/">
+        <a
+          href="https://www.linkedin.com/in/anderson-almendarez/"
+          target="_blank"
+        >
           @Anderson Adonay Almendarez Guzmán
         </a>
-        <a href="https://www.linkedin.com/in/ivon-madrid-277b02ba/">@Ivon</a>
-        <a href="https://www.linkedin.com/in/olga-garc%C3%ADa-cabello-b174b096/">
+        <a
+          href="https://www.linkedin.com/in/ivon-madrid-277b02ba/"
+          target="_blank"
+        >
+          @Ivon Madrid Rodriguez
+        </a>
+        <a
+          href="https://www.linkedin.com/in/olga-garc%C3%ADa-cabello-b174b096/"
+          target="_blank"
+        >
           @Olga García Cabello
         </a>
-        <a href="https://www.linkedin.com/in/jes%C3%BAs-cabral29/">
+        <a
+          href="https://www.linkedin.com/in/jes%C3%BAs-cabral29/"
+          target="_blank"
+        >
           @Jesus Cabral Soares
         </a>
       </div>
